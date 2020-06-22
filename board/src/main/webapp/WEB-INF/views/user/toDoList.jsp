63674af251cb770181b71d1149410607d53871e3
wjfjstkrtm3 <jsy7501@gmail.com>
2020-06-22 21:30:16 +0900

    to do list 구현중..

--- board/src/main/webapp/WEB-INF/views/user/toDoList.jsp
+++ board/src/main/webapp/WEB-INF/views/user/toDoList.jsp
@@ -5,6 +5,7 @@
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 <style type="text/css">
 ul {
 	list-style:none;
@@ -19,19 +20,67 @@ background: #eee;
 padding: 5px 10px;
 }
 
+.checked {
+	text-decoration: line-through;
+	color: #c00;
+	font-weight:bold;
+}
+
+ul li span {
+	padding-left: 10px;
+}
+
+#doneList li {
+	border-bottom: 1px dashed #ccc;
+	margin-bottom:4px;
+	width:30em;
+	height:2.5em;
+	padding: 5px 10px;
+}
+
+#doneList li span {
+	font-size:1.3em;
+	font-weight:200;
+	padding-left:10px;
+	vertical-align:middle;
+	text-decoration:line-through;
+	color:red;
+}
 </style>
 </head>
 <body>
-<h1>To do List 만들기</h1>
-<p>
-	<input type="text" id="inputText">
-	<button id="btnAdd">New Item</button>
-</p>
+<div class="container">
 
-<ul id="todoList">
+
+	<div class="row"> 
+		<a href="#"><h1>To Do List</h1></a>
+	</div>
 	
-</ul>
+	<div class="row">
+		<div class="col-sm-6">
+			<hr>
+			<p id="enter">Write what to do and Press Enter!</p>
+			
+			<p class="inputarea">
+				<input type="text" id="inputText" placeholder="what to do">
+				<ul id="todoList"></ul>
+			</p>
+		</div>
+	
+		<div class="col-sm-6">
+			<hr>
+			<p id="enter">Done</p>
+			
+			<ul id="doneList"></ul>
+		
+		</div>		
+	
+	</div>
 
+
+
+
+</div>
 
 </body>
 <script src="/resources/js/toDoList.js"></script>
--- board/src/main/webapp/resources/js/toDoList.js
+++ board/src/main/webapp/resources/js/toDoList.js
@@ -1,20 +1,81 @@
-var btnNew = document.getElementById("btnAdd");
+/*var btnNew = document.getElementById("btnAdd");*/
+/*var totalItems = 0;*/
+var inputText = document.getElementById("inputText");
+inputText.focus();
+
+var doneList = document.getElementById("doneList");
+
+function moveItem() {
+	var listItem = document.getElementById(this.id);
+	doneList.appendChild(listItem);
+}
 
 function addNewItem(list, itemText) {
+	var date = new Date();
+	var id = "" + date.getHours() + date.getMinutes() + date.getSeconds() + date.getMilliseconds();
+	
 	var listItem = document.createElement("li");
-	listItem.innerText = itemText;
+	listItem.id = 'li_' + id;	
+	listItem.ondblclick = moveItem;
+	
+	var checkBox = document.createElement("input");
+	checkBox.type = "checkBox";
+	checkBox.id = 'cb_' + id;
+	checkBox.onclick = updateItemStatus;
+	
+	var span = document.createElement("span");
+	span.id = 'item_' + id;
+	span.innerText = itemText;
+	span.onclick = renameItem;
+
+	listItem.appendChild(checkBox);
+	listItem.appendChild(span);
 	list.appendChild(listItem);
 }
 
-btnNew.onclick = function() {
-	var inputText = document.getElementById("inputText");
-	var itemText = inputText.value.trim();
+function updateItemStatus() {
+	console.log(this);
+	var chId = this.id.replace("cb_", "");
+	var itemText = document.getElementById("item_" + chId);
+	
+	if(this.checked) {
+		itemText.className = "checked";
+	} else {
+		itemText.className = "";
+	}
+}
 
-	if(itemText === "") {
+function renameItem() {
+	var newText = prompt("다시 지을 이름을 작성하세요");
+	
+	if(newText === "" || newText === " ") {
 		return false;
 	}
+	this.innerText = newText;
 	
-	addNewItem(document.getElementById("todoList"), itemText);
+}
+
+function removeItem() {
+	var listItemId = this.id.replace("li_", "");
+	document.getElementById(this.id).style.display="none";
+	/*document.getElementById("li_" + listItemId).style.display="none";*/
+	
+	
+}
+
+inputText.onkeyup = function(event) {
+/*	console.log(event.which);
+	console.log(event.keyCode);*/
+	if(event.which == 13) {
+		var itemText = inputText.value.trim();
+		if(itemText === "") {
+			return false;
+		}
+		addNewItem(document.getElementById("todoList"), itemText);
+		inputText.focus();
+		inputText.value = "";
+	}
+	
 }
 
 
