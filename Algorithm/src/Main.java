771c588f2eefaeae16e95ed98509352905a3a128
wjfjstkrtm3 <jsy7501@gmail.com>
2020-12-29 19:45:24 +0900

    알고리즘 공부

--- Algorithm/src/Main.java
+++ Algorithm/src/Main.java
@@ -1,20 +1,31 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws Exception {
 		
-		int a = sc.nextInt();
-		int result = 0;
-		if(a % 4 ==0 && (a % 100 != 0 || a % 400 == 0)){
-			result = 1;
-		} else {
-			result = 0;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		String aa = "aaaa";
+		char str = aa.charAt(0);
+		System.out.println(str);
+		
+		while(true) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			sb.append(a+b).append("\n");
+			if(a == 0 && b == 0) {
+				break;
+			}
 		}
-		System.out.println(result);
 		
+		br.close();
+		System.out.println(sb);
 		
-	}
-
+	}	
+	
 }
