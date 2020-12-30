8c92d040276820c94a469fedbf9a187e9b401816
wjfjstkrtm3 <jsy7501@gmail.com>
2020-12-30 21:04:31 +0900

    Scanner, BufferedReader, EOF 등등 공부, 알고리즘 공부

--- Algorithm/src/Main.java
+++ Algorithm/src/Main.java
@@ -1,30 +1,21 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		
+
+		// 나머지
+		Scanner sc = new Scanner(System.in);
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		String aa = "aaaa";
-		char str = aa.charAt(0);
-		System.out.println(str);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
 		
-		while(true) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			
-			sb.append(a+b).append("\n");
-			if(a == 0 && b == 0) {
-				break;
-			}
-		}
+				System.out.println(x + y);
 		
-		br.close();
-		System.out.println(sb);
 		
 	}	
 	
