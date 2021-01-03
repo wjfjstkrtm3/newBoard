9260d534e1f16cbee8ff91e8a28be2b108811dcc
wjfjstkrtm3 <jsy7501@gmail.com>
2021-01-03 20:59:21 +0900

    알고리즘 공부

--- Algorithm/src/Main.java
+++ Algorithm/src/Main.java
@@ -1,53 +1,59 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-
+ 
 public class Main {
-
-	public static void main(String[] args) throws Exception {
-
+	public static void main(String[] args) throws IOException {
+    
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
 		
-		int N = Integer.parseInt(br.readLine());
-		
-		int han = hanCount(N);
-		
-		System.out.println(han);
-	}
-	
-	public static int hanCount(int N) {
-				// 1 ~ 99 까지는 등차수열 한수의 개수는 99개
-				// 100부터는 한수의 개수는 최소 99개이고, 각 자리수를 하나씩 구한다 (백의 자리, 십의 자리, 일의 자리)
-				// 1000은 한수가 아니므로 예외처리를 해줌
-				
-				int han = 0;
-				
-				if(N < 100) {
-					han = N;
-					
-				} else { // N이 100이상이면
-					
-					if(N == 1000) { // N이 1000일때 예외처리
-						N = 999;
-					}
-					
-					han = 99;
-					for(int i = 100; i <= N; i++) {
-						int hun = i / 100; // 211 / 100 = 2
-						int ten = (i / 10)%10; // (211 / 10)%10 = 1
-						int one = i % 10; // 211 % 10 = 1
-						
-						// 공차가 같으면 등차수열임
-						if((hun-ten) == (ten-one)) { // 123  -> (1-2) == (2-3)
-							han++;
-						}
-					}
+		int len = str.length();
+		int count = 0;
+ 
+		for (int i = 0; i < len; i++) {
+ 
+			char ch = str.charAt(i);
+ 
+			if(ch == 'c' && i < len - 1) {			// 만약 ch 가 c 라면?
+				//만약 ch 다음 문자가 '=' 또는 '-' 이라면?
+				if(str.charAt(i + 1) == '=' || str.charAt(i + 1) == '-') {		
+					// i+1 까지가 하나의 문자이므로 다음 문자를 건너 뛰기 위해 1 증가
+					i++;		
 				}
 				
-				
-				
-			return han;
+			}
+		    
+			else if(ch == 'd' && i < len - 1) {
+				if(str.charAt(i + 1) == '-') {	// d- 일 경우
+						i++;
+					}
+				else if(str.charAt(i + 1) == 'z' && i < len - 2) {
+					
+					if(str.charAt(i + 2) == '=') {	// dz= 일 경우
+						i += 2;
+					}
+				}
+			}
+		    
+			else if((ch == 'l' || ch == 'n') && i < len - 1) {
+				if(str.charAt(i + 1) == 'j') {	// lj 또는 nj 일 경우
+					i++;
+				}
+			}
+		    
+ 
+			else if((ch == 's' || ch == 'z') && i < len - 1) {
+				if(str.charAt(i + 1) == '=') {	// s= 또는z= 일 경우
+					i++;
+				}
+			
+		    }
+		    
+			count++;
+ 
+		}
+ 
+		System.out.println(count);
 	}
-	
 }
